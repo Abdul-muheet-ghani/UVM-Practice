ed81651fd97424527e638d6aa1655b9ef2b63ded
Abdul Muheet Ghani <aghani@students.uit.edu>
2022-05-29 17:27:12 +0500

    and  testbench

--- SystemVerilog Practice/test.sv
+++ SystemVerilog Practice/test.sv
@@ -1,11 +1,17 @@
 // Testbench
 // $test$plusargs is used for controlling simulation through command line
 // Avoid the recompilation of testbench
-module test;
+// $value$plusargs use to get the value from command line and assign it to any variable
 
+// Testbench
+module test;
+	bit a;
   task write_data;
     begin
+      if (a == 1) begin
       $display("write data is called ...");
+      end
+      else $display ("a is not equal to 1 in write arguement");
     end
   endtask
   
@@ -22,7 +28,9 @@ module test;
   endtask
   
   initial begin
-    if($test$plusargs("write")) write_data;                //"write is the arguement which return 1'b1 in if block so if arguement is pass through command line i.e "write" then it returns 1'b1 else 1'b0"
+    $value$plusargs("A_value=%d",a);  // A_value is given from command line which is assign to vriable a
+    
+    if($test$plusargs("write")) write_data; //"write is the arguement which return 1'b1 in if block so if arguement is pass through command line i.e "write" then it returns 1'b1 else 1'b0"
     else if($test$plusargs("read")) read_data;
     else if($test$plusargs("READ_WRITE")) read_write_data;
     else $display("nothing is called ....");
