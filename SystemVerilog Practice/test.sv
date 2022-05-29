13d34ed5f83d79543c66887258224e28328d4a34
Abdul Muheet Ghani <aghani@students.uit.edu>
2022-05-29 16:54:33 +0500

    testbench

--- SystemVerilog Practice/test.sv
+++ SystemVerilog Practice/test.sv
@@ -1,4 +1,6 @@
 // Testbench
+// $test$plusargs is used for controlling simulation through command line
+// Avoid the recompilation of testbench
 module test;
 
   task write_data;
@@ -20,7 +22,7 @@ module test;
   endtask
   
   initial begin
-    if($test$plusargs("write")) write_data;
+    if($test$plusargs("write")) write_data;                //"write is the arguement which return 1'b1 in if block so if arguement is pass through command line i.e "write" then it returns 1'b1 else 1'b0"
     else if($test$plusargs("read")) read_data;
     else if($test$plusargs("READ_WRITE")) read_write_data;
     else $display("nothing is called ....");
