4182b32d50980569411b04dc1d0a52e5bf01df98
Abdul Muheet Ghani <aghani@students.uit.edu>
2022-06-05 01:30:05 +0500

    Driver for DUT

--- Adder verification/driver.sv
+++ Adder verification/driver.sv
@@ -2,23 +2,25 @@ class driver;
 
     virtual intf vif;
     mailbox mbx;
+    transaction tr;
     
     function new(virtual intf vif, mailbox mbx);
-        vif = new();
-        this.vif = new();
-        this.mbx = new();
+        tr = new();
+        this.vif = vif;
+        this.mbx = mbx;
     endfunction //new()
 
-    task automatic run();
+    task run();
         forever begin
             mbx.get(tr);
             vif.a = tr.a;
-            vif.a = tr.a;
-            vif.a = tr.a;
+            vif.b = tr.b;
+            vif.cin = tr.cin;
         #5;
             tr.sum = vif.sum;
             tr.carry = vif.carry;
-            $$display("output sum =%b,carry =%b",tr.sum,tr.carry);
+           // $display("output from driver a =%b,b =%b,cin =%b",vif.a,vif.b,vif.cin);
+            $display("output sum =%b,carry =%b",tr.sum,tr.carry);
 
         end
         
