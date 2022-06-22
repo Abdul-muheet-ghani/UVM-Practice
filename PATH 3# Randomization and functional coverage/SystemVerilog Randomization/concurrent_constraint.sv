9f17417a0f2e355ebd0e5c5a124e9ac602f01914
Abdul Muheet Ghani <aghani@students.uit.edu>
2022-06-23 00:00:15 +0500

    constraint solver lecture #3 concurrent example

---
+++ PATH 3# Randomization and functional coverage/SystemVerilog Randomization/concurrent_constraint.sv
@@ -0,0 +1,32 @@
+class concurrent;
+
+    rand bit [2:0] x,y,z;    //for randomization
+
+    constraint conc{
+      x<y;
+      y<10;
+      x<5;
+      x+y == z;             //run concurrently
+        }
+        
+endclass //randomization
+
+module randomize_class_module ;
+
+    concurrent ran_h;
+    int count[int] = '{default:0};
+    initial begin
+        ran_h = new();
+        repeat(10) begin
+          if(ran_h.randomize()) begin                  //if randomization occur
+            $display("x =%d,y =%d,z =%d",ran_h.x,ran_h.y,ran_h.z);
+            count[ran_h.x]++;
+            end
+            else begin
+                $display("error");
+            end
+        end
+        $display(count);
+    end
+    
+endmodule
