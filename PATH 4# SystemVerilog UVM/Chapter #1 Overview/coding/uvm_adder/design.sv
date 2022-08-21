8e89c7a14f2c1146183f89c17535dd8210d997ef
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-08-21 17:54:18 +0000

    Adder design update

--- PATH 4# SystemVerilog UVM/Chapter #1 Overview/coding/uvm_adder/design.sv
+++ PATH 4# SystemVerilog UVM/Chapter #1 Overview/coding/uvm_adder/design.sv
@@ -1,20 +1,24 @@
-// Code your design here
-interface dut_int;
-    logic clk,reset;
-    logic [7:0]data_a;
-    logic [7:0]data_b;
-    logic [7:0]result;
+// This is the SystemVerilog interface that we will use to connect
+// our design to our UVM testbench.
+interface dut_if;
+    logic clock, reset;
+    logic [7:0] data_a;
+    logic [7:0] data_b;
+    logic [7:0] result;
   endinterface
   
   `include "uvm_macros.svh"
   
-  module dut(dut_int inter);
+  // This is our design module.
+  // 
+  // It is an empty design that simply prints a message whenever
+  // the clock toggles.
+  module dut(dut_if dif);
     import uvm_pkg::*;
-    always@(posedge inter.clk) begin
-      if(inter.reset != 1)begin
-        inter.result = inter.data_a + inter.data_b;
-        `uvm_info("DUT",$sformat("Adder result = 0x%h,operand a = 0x%h, operand b = 0x%h,",inter.result,inter.data_a,inter.data_b),UVM_MEDIUM)
+    always @(posedge dif.clock)
+      if (dif.reset != 1) begin
+        dif.result = dif.data_a + dif.data_b;
+        `uvm_info("DUT",$sformatf("Adder result = 0x%h,operand a = 0x%h, operand b = 0x%h,",dif.result,dif.data_a,dif.data_b),UVM_MEDIUM)
       end
-    end
-      
   endmodule
+  
