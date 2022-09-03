434a7d1bc70ff0fde74d9841c671b0a260f72112
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-09-03 15:03:48 +0000

    update Sequencer of Adder UVM

--- PATH 4# SystemVerilog UVM/Chapter #1 Overview/coding/uvm_adder/sequencer.svh
+++ PATH 4# SystemVerilog UVM/Chapter #1 Overview/coding/uvm_adder/sequencer.svh
@@ -1,38 +1,43 @@
+class my_transaction extends uvm_sequence_item;
 
-
-
-
-class tx_sequence extends uvm_sequence_item;
-    `uvm_object_utils(tx_sequence)
-    
+  `uvm_object_utils(my_transaction)
+  
     rand int data_a;
     rand int data_b;
     constraint c_data_a {data_a>=0; data_a<=255;}
     constraint c_data_b {data_b>=0; data_b<=255;}
-    
-    function new(string name ="tx_sequence");
-      super.new(name);
-    endfunction
-  endclass
-  
-  
-  class tx_sequencer extends uvm_sequence;
-    `uvm_component_utils(tx_sequencer)
-    function new(string name, uvm_component parent);
-      super.new(name,parent);
-    endfunction
-        
-    virtual task body;
-      repeat (8) begin
-        req = tx_sequence::type_id::create("req");
-        start_item(req);
-        if(!req.randomize())begin
-          `uvm_error("tx_sequence","failed radomization");
-        end
-        finish_item(req);
+
+  function new (string name = "");
+    super.new(name);
+  endfunction
+
+endclass: my_transaction
+
+class my_sequence extends uvm_sequence#(my_transaction);
+
+  `uvm_object_utils(my_sequence)
+
+  function new (string name = "");
+    super.new(name);
+  endfunction
+
+  task body;
+    repeat(8) begin
+      req = my_transaction::type_id::create("req");
+      start_item(req);
+
+      if (!req.randomize()) begin
+        `uvm_error("MY_SEQUENCE", "Randomize failed.");
       end
-    endtask
-  endclass
-  
-  
-  
+
+      // If using ModelSim, which does not support randomize(),
+      // we must randomize item using traditional methods, like
+      // req.cmd = $urandom;
+      // req.addr = $urandom_range(0, 255);
+      // req.data = $urandom_range(0, 255);
+
+      finish_item(req);
+    end
+  endtask: body
+
+endclass: my_sequence
