2e3090008e5bdf086fd90b3b243d1643788a98d9
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-09-03 15:04:13 +0000

    update Driver of Adder UVM

--- PATH 4# SystemVerilog UVM/Chapter #1 Overview/coding/uvm_adder/driver.svh
+++ PATH 4# SystemVerilog UVM/Chapter #1 Overview/coding/uvm_adder/driver.svh
@@ -1,35 +1,38 @@
+class my_driver extends uvm_driver #(my_transaction);
 
+  `uvm_component_utils(my_driver)
 
+  virtual dut_if dut_vif;
 
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
 
+  function void build_phase(uvm_phase phase);
+    // Get interface reference from config database
+    if(!uvm_config_db#(virtual dut_if)::get(this, "", "dut_vif", dut_vif)) begin
+      `uvm_error("", "uvm_config_db::get failed")
+    end
+  endfunction 
 
-class tx_driver extends uvm_driver #(tx_sequence);
-    `uvm_component_utils(tx_driver)
-  
-   virtual dut_int vdut_int;
-    function new(string name, uvm_component parent);
-      super.new(name,parent);
-    endfunction
+  task run_phase(uvm_phase phase);
+    // First toggle reset
+    dut_vif.reset = 1;
+    @(posedge dut_vif.clock);
+    #1;
+    dut_vif.reset = 0;
     
-      function void build_phase(uvm_phase phase);
-      // Get interface reference from config database
-        if(!uvm_config_db#(virtual dut_int)::get(this, "", "dut_vif", vdut_int)) begin
-        `uvm_error("", "uvm_config_db::get failed")
-      end
-    endfunction
-    
-      task run_phase (uvm_phase phase);
-        vdut_int.reset = 1;
-        @(posedge vdut_int.clk);
-        vdut_int.reset = 0;
-        forever begin
-          set_item_port.get_next_item(req);
-          vdut_int.result = req.result;
-          vdut_int.data_a = req.data_a;
-          vdut_int.data_b = req.data_b;
-          @(posedge vdut_int.clk);
-          seq_item_port.done_item();
-        end
-      endtask
-    
-  endclass
+    // Now drive normal traffic
+    forever begin
+      seq_item_port.get_next_item(req);
+
+      // Wiggle pins of DUT
+      dut_vif.data_a  = req.data_a;
+      dut_vif.data_b = req.data_b;
+      @(posedge dut_vif.clock);
+
+      seq_item_port.item_done();
+    end
+  endtask
+
+endclass: my_driver
