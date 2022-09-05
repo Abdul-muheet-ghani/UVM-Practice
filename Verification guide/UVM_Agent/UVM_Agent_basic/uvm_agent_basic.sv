036663a6da477d847657c26faf339b39216f32f8
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-09-05 15:17:16 +0000

    Sequence_item + Sequence + driver + agent

--- Verification guide/UVM_Agent/UVM_Agent_basic/uvm_agent_basic.sv
+++ Verification guide/UVM_Agent/UVM_Agent_basic/uvm_agent_basic.sv
@@ -42,17 +42,22 @@ endclass
 
 //===============================================================
 
-class sequencer extends uvm_sequence #(sequence_item);
-    `uvm_object_utils(sequencer)
+class sequence_class extends uvm_sequence #(sequence_item);
+    `uvm_object_utils(sequence_class)
 
     function new(string name = "sequence_item");
         super.new(name);
     endfunction
 	sequence_item seq0;
     virtual task body();
-      sequence_item seq0;
         seq0 = sequence_item::type_id::create("seq0");
         start_item(seq0);
+        finish_item(seq0);
+      $display("end=========================================0");
+      seq0 = sequence_item::type_id::create("seq0");
+      $display("end=========================================1");
+        start_item(seq0);
+      $display("end=========================================2");
       finish_item(seq0);
     endtask
 endclass
@@ -66,24 +71,24 @@ class uvm_agnt extends uvm_agent;
         super.new(name,parent);
     endfunction
 
-  uvm_sequencer#(sequence_item) sqr;
+  uvm_sequencer#(sequence_item) sequencer_h;
     driver_seq drv;
 
     function void build_phase(uvm_phase phase);
-        sqr = uvm_sequencer#(sequence_item)::type_id::create("sqr",this);//create the object of sequencer
+        sequencer_h = uvm_sequencer#(sequence_item)::type_id::create("sequencer_h",this);//create the object of sequencer
         drv = driver_seq::type_id::create("drv",this);
     endfunction
 
     function void connect_phase(uvm_phase phase);
-        drv.seq_item_port.connect(sqr.seq_item_export);                //TLM connection driver to parent of uvm_sequencer
+        drv.seq_item_port.connect(sequencer_h.seq_item_export);           //TLM connection driver to parent of uvm_sequencer
     endfunction
 
     task run_phase (uvm_phase phase);
       phase.raise_objection(this);
       begin
-        sequencer sq;                                                  //sequencer instance
-        sq = sequencer::type_id::create("sq");                         //create copy and clone
-        sq.start(sqr);                                                 //start the phases like, create, w8 for grant, randomize and other
+        sequence_class sequence_h;                                        //sequence instance
+        sequence_h = sequence_class::type_id::create("sequence_h");       //create copy and clone
+        sequence_h.start(sequencer_h);                                    //start the phases like, create, w8 for grant, randomize and other
       end
       phase.drop_objection(this);
     endtask
@@ -96,7 +101,7 @@ module test;
     //uvm_agnt agnt;
     initial begin
       $display("end=================");
-      run_test("uvm_agnt");                                             //class agent run
+      run_test("uvm_agnt");                                               //class agent run
       $display("end");
     end
 endmodule
